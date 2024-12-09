bb77633189247cc1e64649018aa75ce73ac60a7f
oructuran0 <orctrn193@gmail.com>
2024-12-09 23:08:25 +0300

    Entity Framework Core, Desing, Sql Server and Tools packages added to Entity Framework layer

--- MyApiNight/MyApiNight.EntityLayer/obj/Debug/net6.0/MyApiNight.EntityLayer.AssemblyInfo.cs
+++ MyApiNight/MyApiNight.EntityLayer/obj/Debug/net6.0/MyApiNight.EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiNight.EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4f0cb4d54f7a7cca2fb79c7b1de086e5cb4a87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f010e58c74e5763bec3f2faba0605926eaf987")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiNight.EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiNight.EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
